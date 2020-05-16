0c1adc272256891940a00396a0719833ca2dfb6f
dorothyshek <32554289+dorothyshek@users.noreply.github.com>
2020-05-16 19:08:34 -0400

    Update results.py

--- results/results.py
+++ results/results.py
@@ -2,14 +2,12 @@ import folium
 import pandas as pd
 
 df = pd.read_csv('african_countries.csv')
-data = pd.read_csv('example_data.csv')
+data = pd.read_csv('results.csv')
 
 coordinates = (8.7832, 34.5085) # starting position of map: African continent
 map = folium.Map(location=coordinates, zoom_start=3)
 
-#df = df.dropna(how='all', axis='columns').dropna(axis=0)
-
-comb = df.join(data.set_index('CountryCode'), on='CountryCode') # join on key=CountryCode
+comb = df.join(data.set_index('Country'), on='Country') # join on key=Country
 
 african_countries = {'Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 'Cameroon', 'Cape Verde', 'Central African Republic', 'Chad', 'Comoros', 
     'Congo, The Democratic Republic of the', 'Congo', 'Djibouti', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 
@@ -20,15 +18,16 @@ african_countries = {'Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', '
 remove = {'Botswana', 'Comoros', 'Congo, The Democratic Republic of the', 'Djibouti', 'Equatorial Guinea', 'Gabon', 'Libya', 'Mauritius', 'Sao Tome and Principe', 'Seychelles', 'Somalia', 'South Africa', 'South Sudan'}
 
 keep = african_countries.difference(remove)
-df = comb[comb['Country'].isin(keep)]
+df = comb[comb['Country'].isin(keep)] # only keep rows in set keep
+df = df.dropna() # drop rows that have missing data
 
+
+# hover over countries to see most highly correlated crop
+# with respect to country's food availability
 for i in df.index:
 	country = df['Country'][i]
 	loc = (df['Latitude'][i], df['Longitude'][i])
 	info = '<i>' + country + '</i><br><br>Most correlated crop: ' + df['Crop'][i] + '<br><br>Correlation: ' + str(df['Correlation'][i])
 	folium.Marker(location=loc, popup=info, tooltip=country).add_to(map)
-
-# hover over countries to see most highly correlated crop
-# with respect to country's food availability
 
 map.save('index.html')
